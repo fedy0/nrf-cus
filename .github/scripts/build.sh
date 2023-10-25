fdabd7bf23e32dad1805d4cdda03f8ce7cb1366a
fedy0 <ifedioraelochukwuc@gmail.com>
2023-10-25 21:22:40 +0200

    update sdk url

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -14,6 +14,13 @@ set -e
 #echo "GNU_INSTALL_ROOT ?= ${ARM_GCC_TOOL_IN_HOST}" >> ${NRF_SDK_ROOT_PATH}/components/toolchain/gcc/Makefile.posix
 #echo "GNU_VERSION ?= ${ARM_GCC_VERSION}" >> ${NRF_SDK_ROOT_PATH}/components/toolchain/gcc/Makefile.posix
 
+# Choose NRF SDK to use
+export NRF_SDK=nRF5_SDK_15.3.0_59ac345
+export NRF_SDK_VERSION=nRF5_SDK_v15.x.x
+export NRF_SDK_DOWNLOAD_URL="https://developer.nordicsemi.com/nRF5_SDK/${NRF_SDK_VERSION}/${NRF_SDK}.zip"
+export NRF_SDK_ROOT=nrf_sdk
+export NRF_SDK_ROOT_PATH=${NRF_SDK_ROOT}/${NRF_SDK}
+
 export GNU_INSTALL_ROOT="`dirname $(which arm-none-eabi-gcc)`/"
 export ARM_GCC_VERSION="`arm-none-eabi-gcc --version | awk 'NR==1{ print $3 }'`"
 
